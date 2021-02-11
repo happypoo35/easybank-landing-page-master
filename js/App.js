b5e9811832196ed301f54adf44ea6b77d11afe80
Alex <happypoo35@gmail.com>
2021-02-11 18:20:09 +0300

    first commit

---
+++ js/App.js
@@ -0,0 +1,26 @@
+const hamburgerBtn = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+const body = document.querySelector("body");
+
+hamburgerBtn.addEventListener("click", () => {
+  hamburgerBtn.firstElementChild.classList.toggle("active");
+  body.style.overflow = body.style.overflow === "hidden" ? "" : "hidden";
+  body.classList.toggle("active");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === hamburgerBtn || e.target.parentNode === hamburgerBtn) return;
+  if (e.target !== menu) {
+    hamburgerBtn.firstElementChild.classList.remove("active");
+    body.classList.remove("active");
+    body.style.overflow = "";
+  }
+});
+
+window.onresize = () => {
+  if (window.innerWidth >= 580) {
+    hamburgerBtn.firstElementChild.classList.remove("active");
+    body.classList.remove("active");
+    body.style.overflow = "";
+  }
+};
