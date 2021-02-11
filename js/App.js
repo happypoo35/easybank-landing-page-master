0a2ea0d78e8c6693bd7154c9a7e05bd325378b5e
Alex <happypoo35@gmail.com>
2021-02-11 18:54:22 +0300

    menu toggle updated

--- js/App.js
+++ js/App.js
@@ -2,9 +2,21 @@ const hamburgerBtn = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
 const body = document.querySelector("body");
 
+// console.log(menu.style.display === "");
+
+// const menuToggle = () => {
+//   if (menu.style.display) {
+//     console.log("hello");
+//   } else {
+//     console.log("goodbye");
+//   }
+// };
+// menuToggle();
+
 hamburgerBtn.addEventListener("click", () => {
   hamburgerBtn.firstElementChild.classList.toggle("active");
   body.style.overflow = body.style.overflow === "hidden" ? "" : "hidden";
+  menu.style.display = menu.style.display === "" || "none" ? "flex" : "none";
   body.classList.toggle("active");
 });
 
